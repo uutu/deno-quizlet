939c12cdbbe2e08ade1212cfa05103cb4aa1de1f
uutu <41518314+uutu@users.noreply.github.com>
2022-10-17 18:57:47 +0300

    Delete drill-and-practice/tests/drill-and-practice directory

--- drill-and-practice/tests/drill-and-practice/app_test.js
+++
@@ -1,9 +0,0 @@
-import { app } from "../../app.js";
-import { superoak } from "../../deps.js";
-
-Deno.test("Sending a simple GET request should return status code 200", async () => {
-    //const app = new Application();
-    const testClient = await superoak(app);
-    await testClient.get("/")
-        .expect(200);
-});
