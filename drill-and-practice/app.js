50e611ca3bd42f77271e5c54e910290ddd83fc63
Juuso-Petteri Kuisma <juuso.kuisma@gmail.com>
2022-10-21 20:30:49 +0300

    Take authMiddleware to use in app

--- drill-and-practice/app.js
+++ drill-and-practice/app.js
@@ -1,4 +1,5 @@
 import { Application, Session } from "./deps.js";
+import { authMiddleware } from "./middlewares/authMiddleware.js";
 import { errorMiddleware } from "./middlewares/errorMiddleware.js";
 import { renderMiddleware } from "./middlewares/renderMiddleware.js";
 import { serveStaticMiddleware } from "./middlewares/serveStaticMiddleware.js";
@@ -9,6 +10,7 @@ const app = new Application();
 const session = new Session();
 app.use(session.initMiddleware());
 
+app.use(authMiddleware);
 app.use(errorMiddleware);
 app.use(serveStaticMiddleware);
 app.use(renderMiddleware);
