3740e3e3283245a8f2bc7e6172400b8728cdf6d4
Juuso-Petteri Kuisma <juuso.kuisma@gmail.com>
2022-10-24 15:33:50 +0300

    Add feature for admins to add topics

--- drill-and-practice/app.js
+++ drill-and-practice/app.js
@@ -1,4 +1,5 @@
 import { Application, Session } from "./deps.js";
+import { authMiddleware } from "./middlewares/authMiddleware.js";
 import { errorMiddleware } from "./middlewares/errorMiddleware.js";
 import { renderMiddleware } from "./middlewares/renderMiddleware.js";
 import { serveStaticMiddleware } from "./middlewares/serveStaticMiddleware.js";
@@ -10,6 +11,7 @@ const session = new Session();
 app.use(session.initMiddleware());
 
 app.use(errorMiddleware);
+app.use(authMiddleware);
 app.use(serveStaticMiddleware);
 app.use(renderMiddleware);
 app.use(router.routes());
--- drill-and-practice/middlewares/renderMiddleware.js
+++ drill-and-practice/middlewares/renderMiddleware.js
@@ -1,11 +1,20 @@
 import { configure, renderFile } from "../deps.js";
 
-const renderMiddleware = async (context, next) => {
-  configure({
-    views: `${Deno.cwd()}/views/`,
-  });
+configure({
+  views: `${Deno.cwd()}/views/`,
+});
 
+const renderMiddleware = async (context, next) => {
+  
   context.render = async (file, data) => {
+    if (!data) {
+      data = {};
+    }
+
+    if (context.user) {
+      data.user = context.user;
+    }
+    
     context.response.headers.set("Content-Type", "text/html; charset=utf-8");
     context.response.body = await renderFile(file, data);
   };
