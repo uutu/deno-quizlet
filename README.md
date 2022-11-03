79994ba8e53256b19354db63cec0f17ddfe73eaf
Juuso-Petteri Kuisma <juuso.kuisma@gmail.com>
2022-11-03 12:55:49 +0200

    Add tests for API

--- README.md
+++ README.md
@@ -11,3 +11,18 @@
 
 
 ## Deployed application url:
+
+
+## Running tests
+
+The project has quite a number of automated end-to-end tests and has a CI-CD pipeline
+set up for automated Heroku deployment when the tests pass. To run the tests locally,
+use the command docker compose run --rm drill-and-practice test --allow-all
+while within the directory that contains the docker-compose.yml -file.
+
+Depending on your installation (given that either Docker Compose or Docker Desktop is installed), the command might
+be docker-compose run --rm drill-and-practice test --allow-all.
+
+- The --rm flag runs a one-off command on a service that is discarded after use.
+- drill-and-practice is the service/application's name
+- the --allow-all flag can be substituted with --allow-env --allow-net --allow-read if you want to limit access
--- drill-and-practice/tests/routes/apis/quizApi_test.js
+++ drill-and-practice/tests/routes/apis/quizApi_test.js
@@ -1,7 +1,27 @@
 import { app } from "../../../app.js";
 import { superoak } from "../../../deps.js";
 
-Deno.test("it will allow you to make assertions if you await it", async () => {
-    const request = await superoak(app);
-    await request.get("/").expect(200).expect("Hello Deno!");
-  });
+Deno.test({
+    name: "GET request to API route /api/questions/random returns status 200 and a JSON object {}",
+    async fn() {
+        const request = await superoak(app);
+        await request.get("/api/questions/random")
+            .expect(200)
+            .expect("Content-Type", "application/json; charset=utf-8");
+    },
+    sanitizeResources: false,
+    sanitizeOps: false,
+});
+
+Deno.test({
+    name: "POST request to API route /api/questions/answer with bogus data returns status 404",
+    async fn() {
+        const request = await superoak(app);
+        await request.post("/api/questions/answer")
+        .set("Content-Type", "application/json")
+        .send('{"questionId":"9999", "optionId":"9999"}')
+        .expect(404)
+    },
+    sanitizeResources: false,
+    sanitizeOps: false,
+});
